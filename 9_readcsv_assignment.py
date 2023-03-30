f072bdab674f7cc4ce3e52acc19023b0cc5b3aa7
Beatrice <felicia.beatrice99@gmail.com>
2023-03-30 16:29:03 +0800

    assignment 5

---
+++ 9_readcsv_assignment.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 1.Input
+raw_data = pd.read_csv("readcsv_assignment.csv")
+print(raw_data)
+
+# 2. Process
+Numberofitems = len(raw_data)
+Sorted_data = raw_data.sort_values ("Menu",ascending=True)
+
+# 3. Output
+print(f"Count:{Numberofitems}")
+print(f'{Sorted_data}')
